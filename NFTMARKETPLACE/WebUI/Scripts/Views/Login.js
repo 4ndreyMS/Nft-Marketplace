f07283685eac94566d12f37225d62d2268e00c76
Andrey M <amelendezs@ucenfotec.ac.cr>
2022-04-18 19:19:36 -0600

    commit restricting the log to the unactive user and changing the log for email

--- NFTMARKETPLACE/WebUI/Scripts/Views/Login.js
+++ NFTMARKETPLACE/WebUI/Scripts/Views/Login.js
@@ -1,65 +1,92 @@
-﻿function Login() {
+﻿
+function Login() {
 
     //controller que va a buscar en el api
-    this.ServiceController = 'User';
-    this.ServiceControllerUserRole = 'RoleUser';
-    this.ServicePass = 'Loggin';
-    this.ctrlActions = new ControlActions();
+    const ServiceController = 'User';
+    const ServiceControllerUserRole = 'RoleUser';
+    const ServicePass = 'Loggin';
+    const ctrlActions = new ControlActions();
+
+    this.validateUser = function () {
+
+    }
 
     this.LoginUser = function () {
 
-
+        let RoleUser = { UserId: "" };
         let validation = false;
         sessionStorage.removeItem('UserCedula');
-        //lee la informacion del form con el id = frmLogin
-        var userLog = this.ctrlActions.GetDataForm("frmLogin");
+        sessionStorage.removeItem('UserCompany');
 
-        if (userLog.UserId === "" || userLog.UserPass === "") {
+        //lee la informacion del form con el id = frmLogin
+        var userLog = ctrlActions.GetDataForm("frmLogin");
+        let validMail = false;
+        let userRequest = { Email: userLog.UserEmail, Status: "" };
+        let UserPass = { Cedula: "", Password: userLog.UserPass }
+
+
+        if (userLog.UserEmail === "" || userLog.UserPass === "") {
             alert("FILL ALL THE BLANKS");
         } else {
-            //creacion de un objeto personalizado
-            var UserPass = { Cedula: userLog.UserId, Password: userLog.UserPass }
 
-            var RoleUser = { UserId: userLog.UserId }
+            //verifica que exista un usuario con ese mismo correo
+            ctrlActions.PostToAPI(ServiceController + "/RetriveUserByMail", userRequest, function (response) {
+                console.log(response);
+                if (response != null) {
+                    UserPass.Cedula = response.Cedula;
+                    userRequest.Status = response.Status;
+                    sessionStorage.setItem("UserCompany", response.IdOrganization);
+                    validMail = true;
+                } else {
+                    validMail = false;
+                }
+                //verfica que el usuario esté activo y que si exista el correo
+                if (validMail && userRequest.Status === "Active") {
 
-            this.ctrlActions.PostToAPI(this.ServicePass + "/RetrieveLoggin", UserPass, function (response) {
+                    //consulta la clave con el id del usuario con el correo
+                    ctrlActions.PostToAPI(ServicePass + "/RetrieveLoggin", UserPass, function (response) {
 
-                if (response === 1) {
-                    sessionStorage.setItem("UserCedula", UserPass.Cedula);
-                    validation = true;
-                    //alert("Login sucessfull");
+                        if (response === 1) {
+                            sessionStorage.setItem("UserCedula", UserPass.Cedula);
+                            RoleUser.UserId = UserPass.Cedula;
+                            validation = true;
+                            //alert("Login sucessfull");
 
-                } else if (response === 0) {
-                    validation = false;
-                    alert("Incorrect User Id or Password, Try again!");
+                        } else {
+                            validation = false;
+                            alert("Incorrect Password, Try again!");
+
+                        }
+
+                        if (validation) {
+                            //se realiza el post a un api si la validasion es verdera
+                            ctrlActions.PostToAPI( ServiceControllerUserRole + "/RetriveUserRoleByUserId", RoleUser, function (response) {
+
+
+                                if (response === 1) {
+                                    window.location.href = "Manager";
+                                } else if (response === 2) {
+
+                                    window.location.href = "Profile";
+
+
+                                } else if (response === 3) {
+
+                                    window.location.href = "Profile";
+                                }
+
+
+                            });
+                        }
+
+                    });
+                } else {
+                    alert("Incorrect User name or Password, Try again!");
 
                 }
 
             });
 
-            //se realiza el post a un api
-            this.ctrlActions.PostToAPI(this.ServiceControllerUserRole + "/RetriveUserRoleByUserId", RoleUser, function (response) {
-
-                if (validation === true) {
-
-
-                    if (response === 1) {
-                        window.location.href = "Manager"
-                    } else if (response === 2) {
-
-                        window.location.href = "Profile"
-
-
-                    } else if (response === 3) {
-
-                        window.location.href = "Profile"
-                    }
-
-                }
-            });
-
-
-            
 
         }
     }
